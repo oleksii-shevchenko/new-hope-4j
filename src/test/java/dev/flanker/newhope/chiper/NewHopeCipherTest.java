6df59c4f3dde9601d5fd53e7d5fb86ea6e41c3e5
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2019-12-26 17:10:00 +0200

    Made test repeated

--- src/test/java/dev/flanker/newhope/chiper/NewHopeCipherTest.java
+++ src/test/java/dev/flanker/newhope/chiper/NewHopeCipherTest.java
@@ -2,7 +2,7 @@ package dev.flanker.newhope.chiper;
 
 import dev.flanker.newhope.api.domain.KeyPair;
 import dev.flanker.newhope.spec.NewHopeSpec;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import java.security.SecureRandom;
 import java.util.concurrent.ThreadLocalRandom;
@@ -10,7 +10,9 @@ import java.util.concurrent.ThreadLocalRandom;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class NewHopeCipherTest {
-    @Test
+    private static final int RUNS_NUMBER = 32;
+
+    @RepeatedTest(RUNS_NUMBER)
     public void consistencyTest() {
         NewHopeCipher cipher = new NewHopeCipher(new SecureRandom(), NewHopeSpec.NEW_HOPE_512);
         KeyPair keyPair = cipher.generateKeyPair();
